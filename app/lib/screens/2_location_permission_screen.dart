565b09911117044cb9dbd30c481073771d226020
shashank790 <shashank790@ucsb.edu>
2025-11-09 19:45:22 -0800

    init flutter app with dir structure, mock first 2 of 10 pages

---
+++ app/lib/screens/2_location_permission_screen.dart
@@ -0,0 +1,14 @@
+//placeholder for now
+import 'package:flutter/material.dart';
+
+class LocationPermissionScreen extends StatelessWidget {
+  const LocationPermissionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Allow Location')),
+      body: const Center(child: Text('Location Permission Screen Placeholder')),
+    );
+  }
+}
